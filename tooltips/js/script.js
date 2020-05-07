3c773cb7a2dc0f2d7ec6fff9c5bb5378ad3d2eaf
Hachibkhan <hachib.khan19@gmail.com>
2020-05-07 13:25:41 +0600

    bootstrap

---
+++ tooltips/js/script.js
@@ -0,0 +1,13 @@
+$('[data-toggle="tooltip"]').tooltip();
+
+function showToolTip(){
+    $('#tool').tooltip('show')
+}
+function hideToolTip(){
+    $('#hid').tooltip('hide')
+}
+
+
+function toggleToolTip(){
+    $("#toggle").tooltip("toggle");
+}
